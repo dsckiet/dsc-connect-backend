40bf2a7f347b15fcc17ae3eff4891385fd874d66
ShashankJaitly <shashank16vasu@gmail.com>
2020-03-03 17:54:23 +0530

    --update alter login post response

--- dsc_connect_app/urls.py
+++ dsc_connect_app/urls.py
@@ -1,6 +1,6 @@
 
 
-from dsc_connect_app.views import DscViewSet, UserViewset
+from dsc_connect_app.views import DscViewSet, UserViewset, LoginView
 from rest_framework.routers import DefaultRouter
 from rest_framework_jwt.views import obtain_jwt_token
 from django.conf.urls import url
@@ -10,5 +10,5 @@ router.register(r'v1/dsc/', DscViewSet, basename='dsc')
 router.register(r'v1/users/', UserViewset, basename = 'users')
 urlpatterns = router.urls
 urlpatterns += [
-    url(r'^login/', obtain_jwt_token),
+    url(r'^login/', LoginView.as_view(), name = 'login'),
     ]
--- dsc_connect_app/views.py
+++ dsc_connect_app/views.py
@@ -1,10 +1,19 @@
 from .models import Dsc, User
 
 from .serializers import DscSerializer, UserSerializer
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 
 from django_filters.rest_framework import DjangoFilterBackend
 from django_filters import rest_framework
+
+from rest_framework_jwt.settings import api_settings
+from rest_framework_jwt.views import ObtainJSONWebToken
+
+jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+jwt_decode_handler = api_settings.JWT_DECODE_HANDLER
+
 
 class DscFilter(rest_framework.FilterSet):
 	domains = rest_framework.CharFilter(lookup_expr='icontains')
@@ -20,8 +29,111 @@ class DscViewSet(viewsets.ModelViewSet):
     filterset_class = DscFilter
     permission_class = []
 
+    # def list(self, request):
+    #     return Response({
+    #     	'data':,
+    #     	'error':,
+    #     	'msg':,
+    #     	})
+
+    # def create(self, request):
+    #     pass
+
+    # def retrieve(self, request, pk=None):
+    #     pass
+
+    # def update(self, request, pk=None):
+    #     pass
+
+    # def partial_update(self, request, pk=None):
+    #     pass
+
+    # def destroy(self, request, pk=None):
+    #     pass
+
 class UserViewset(viewsets.ModelViewSet):
 	serializer_class = UserSerializer
 	queryset = User.objects.all()
 	permission_class = []
-	
+
+	# def list(self, request):
+ #        queryset = User.objects.all()
+ #        serializer = UserSerializer(queryset, many=True)
+ #        return Response({
+ #        	'data':,
+ #        	'error':,
+ #        	'msg':,
+
+ #        	})
+
+ #    def create(self, request):
+ #        pass
+
+ #    def retrieve(self, request, pk=None):
+ #        pass
+
+ #    def update(self, request, pk=None):
+ #        pass
+
+ #    def partial_update(self, request, pk=None):
+ #        pass
+
+ #    def destroy(self, request, pk=None):
+ #        pass
+
+
+
+class LoginView(ObtainJSONWebToken):
+    
+    def post(self, request, *args, **kwargs):
+        # by default attempts username / passsword combination
+        response = super(LoginView, self).post(request, *args, **kwargs)
+        # token = response.data['token']  # don't use this to prevent errors
+        # below will return null, but not an error, if not found :)
+        res = response.data
+        token = res.get('token')
+
+        # token ok, get user
+        if token:
+            user = jwt_decode_handler(token)  # aleady json - don't serialize
+        else:  # if none, try auth by email
+            req = request.data  # try and find email in request
+            email = req.get('email')
+            password = req.get('password')
+            #username = req.get('username')
+
+            if email is None or password is None:
+                return Response({
+                                'error': True, 
+                                'message': 'Missing or incorrect credentials',
+                                'data': req},
+                                status=status.HTTP_400_BAD_REQUEST)
+
+            # email exists in request, try to find user
+            try:
+                user = User.objects.get(email=email)
+            except:
+                return Response({
+                                'error': True, 
+                                'message': 'User not found',
+                                'data': req},
+                                status=status.HTTP_404_NOT_FOUND)
+
+            if not user.check_password(password):
+                return Response({'error': True, 
+                                'message': 'Incorrect password',
+                                'data': req},
+                                status=status.HTTP_403_FORBIDDEN)
+
+            # make token from user found by email
+            payload = jwt_payload_handler(user)
+            token = jwt_encode_handler(payload)
+            user = UserSerializer(user).data
+
+        return Response({
+                        'error': False,
+                        'message': 'Successfully logged in',
+                        'token': token,
+                        'user': user }, 
+                        status=status.HTTP_200_OK)
+#You can change the default to check by email only if you please by customising Django's auth model, but I was happy to have both options.	
