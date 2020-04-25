3084df3e6e5fdec3c2771ad7729e3009a3337a8b
ShashankJaitly <shashank16vasu@gmail.com>
2020-04-25 12:26:49 +0530

    --update settings

--- dsc_connect_backend/settings.py
+++ dsc_connect_backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '_hix4**qe3#b4#5+&btu8bhmxy-n08#$)ooc-f5oeq*l3*@x4w'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost']
+ALLOWED_HOSTS = ['https://dsc-connect-backend.herokuapp.com/','localhost']
 
 
 # Application definition
